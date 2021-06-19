9370f05fce49eaeefc21e4fa0ad06039266a3602
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-06-19 15:46:44 +0200

    add container

--- src/app/task2/task2.component.ts
+++ src/app/task2/task2.component.ts
@@ -13,7 +13,7 @@ export class Task2Component implements OnInit {
     new Student(3,"Adel",30,"../assets/up/3.jpg"),
     new Student(4,"Moaz",19,"../assets/up/4.jpg"),
     new Student(5,"khalifa",10,"../assets/up/6.jpg"),
-    new Student(6,"khalifa",10,"../assets/up/7.jpg")
+    new Student(6,"khalifa",40,"../assets/up/7.jpg")
   ]
   constructor() { }
 
