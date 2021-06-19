5787b2afb05bf778c8ca86a63ca20534f23d266d
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-06-19 15:33:34 +0200

    task2 crud operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { Task2Component } from './task2/task2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    Task2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task2/task2.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../_model/student';
+
+@Component({
+  selector: 'app-task2',
+  templateUrl: './task2.component.html',
+  styleUrls: ['./task2.component.css']
+})
+export class Task2Component implements OnInit {
+  students:Student[]=[
+    new Student(1,"Mohamed",24,"../assets/up/1.jpg"),
+    new Student(2,"Ali",26,"../assets/up/2.jpg"),
+    new Student(3,"Adel",30,"../assets/up/3.jpg"),
+    new Student(4,"Moaz",19,"../assets/up/4.jpg"),
+    new Student(5,"khalifa",10,"../assets/up/6.jpg"),
+    new Student(6,"khalifa",10,"../assets/up/7.jpg")
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
